41e1f13a9bdda735224f3d9e176bc37c79690446
TheKillerRex27 <alexnwafor27@gmail.com>
2023-09-04 17:20:21 +0100

    File editing and built structure of the crew page

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import '../stylesheets/Crew.css'
+import commander from '../assets/crew/image-douglas-hurley.png'
 
 function Crew() {
 
     return (
-        <h1 style={{color: 'white'}}>Crew</h1>
+        <section className="crew-page">
+            <div className="crew-details">
+                <p className="header"><span>02</span> MEET YOUR CREW</p>
+                <div className="crew-info">
+                    <p className="crew-title">COMMANDER</p>
+                    <p className="crew-name">DOUGLAS HURLEY</p>
+                    <p className="crew-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                </div>
+                <div className="crew-nav">
+                    <div className="crew-nav-button active-button"></div>
+                    <div className="crew-nav-button"></div>
+                    <div className="crew-nav-button"></div>
+                    <div className="crew-nav-button"></div>
+                </div>
+            </div>
+            <div className="crew-img-section">
+                <img src={commander} className="crew-img" />
+            </div>
+        </section>
     )
 
 }
