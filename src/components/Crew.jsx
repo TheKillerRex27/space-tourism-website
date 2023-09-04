58a171c58e2d0cc5ea8182c0906af5fe6a3c3b9c
TheKillerRex27 <alexnwafor27@gmail.com>
2023-09-04 19:06:02 +0100

    Finished crew page

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import '../stylesheets/Crew.css'
-import commander from '../assets/crew/image-douglas-hurley.png'
+import crew from '../data/crew.json'
 
 function Crew() {
+
+    const douglas = crew.crew[0];
+    const mark = crew.crew[1];
+    const victor = crew.crew[2];
+    const anousheh = crew.crew[3];
+
+    const [currentCrew, setCurrentCrew] = useState(douglas);
+
+    const selectCrew = (crew) => {
+        switch (crew) {
+            case 'Douglas':
+                setCurrentCrew(douglas);
+                break;
+            case 'Mark':
+                setCurrentCrew(mark);
+                break;
+            case 'Victor':
+                setCurrentCrew(victor);
+                break;
+            case 'Anousheh':
+                setCurrentCrew(anousheh);
+                break;
+            default:
+                setCurrentCrew(douglas);
+        }
+    };
 
     return (
         <section className="crew-page">
             <div className="crew-details">
                 <p className="header"><span>02</span> MEET YOUR CREW</p>
                 <div className="crew-info">
-                    <p className="crew-title">COMMANDER</p>
-                    <p className="crew-name">DOUGLAS HURLEY</p>
-                    <p className="crew-text">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                    <p className="crew-title">{currentCrew.role.toUpperCase()}</p>
+                    <p className="crew-name">{currentCrew.name.toUpperCase()}</p>
+                    <p className="crew-text">{currentCrew.bio}</p>
                 </div>
                 <div className="crew-nav">
-                    <div className="crew-nav-button active-button"></div>
-                    <div className="crew-nav-button"></div>
-                    <div className="crew-nav-button"></div>
-                    <div className="crew-nav-button"></div>
+                    <div className={currentCrew === douglas ? 'active-button' : 'crew-nav-button'} onClick={() => selectCrew('Douglas')}></div>
+                    <div className={currentCrew === mark ? 'active-button' : 'crew-nav-button'} onClick={() => selectCrew('Mark')}></div>
+                    <div className={currentCrew === victor ? 'active-button' : 'crew-nav-button'} onClick={() => selectCrew('Victor')}></div>
+                    <div className={currentCrew === anousheh ? 'active-button' : 'crew-nav-button'} onClick={() => selectCrew('Anousheh')}></div>
                 </div>
             </div>
             <div className="crew-img-section">
-                <img src={commander} className="crew-img" />
+                <img src={currentCrew.images.png} className="crew-img" />
             </div>
         </section>
     )
