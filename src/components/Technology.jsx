488103158a3599157615731e820c36160625f825
TheKillerRex27 <alexnwafor27@gmail.com>
2023-09-05 15:52:34 +0100

    Finished Technology Page

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, HeadSpan } from "./styled-components/Header";
 import '../stylesheets/Technology.css'
-import rocket from '../assets/technology/image-launch-vehicle-portrait.jpg'
+import tech from '../data/technology.json'
 
 function Technology() {
+
+    const launch = tech.technology[0]
+    const spaceport = tech.technology[1]
+    const capsule = tech.technology[2]
+
+    const [currentTech, setCurrentTech] = useState(launch);
+
+    const selectTech = (technology) => {
+        switch (technology) {
+            case 'Launch':
+                setCurrentTech(launch);
+                break;
+            case 'Spaceport':
+                setCurrentTech(spaceport);
+                break;
+            case 'Capsule':
+                setCurrentTech(capsule);
+                break;
+            default:
+                setCurrentTech(launch);
+        }
+    };
 
     return (
         <section className="tech-page">
@@ -11,19 +33,19 @@ function Technology() {
                 <Header><HeadSpan>03</HeadSpan> SPACE LAUNCH 101</Header>
                 <div className="tech-info">
                     <div className="nav-buttons">
-                        <div className="active-nav-button"><p>1</p></div>
-                        <div><p>2</p></div>
-                        <div><p>3</p></div>
+                        <div className={ currentTech == launch ? "active-nav-button" : "" } onClick={() => selectTech('Launch')} ><p>1</p></div>
+                        <div className={ currentTech == spaceport ? "active-nav-button" : "" } onClick={() => selectTech('Spaceport')} ><p>2</p></div>
+                        <div className={ currentTech == capsule ? "active-nav-button" : "" } onClick={() => selectTech('Capsule')} ><p>3</p></div>
                     </div>
                     <div>
                         <p className="tech-header">THE TERMINOLOGY...</p>
-                        <p className="tech-title">LAUNCH VEHICLE</p>
-                        <p className="tech-desc">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                        <p className="tech-title">{currentTech.name.toUpperCase()}</p>
+                        <p className="tech-desc">{currentTech.description}</p>
                     </div>
                 </div>
             </div>
             <div>
-                <img src={rocket} className="tech-img" />
+                <img src={currentTech.images.portrait} className="tech-img" />
             </div>
         </section>
     )
